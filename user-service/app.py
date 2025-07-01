7f9263ceac49e5d79740e95933b13475357d6862
Bharathimula <bharathim273@gmail.com>
2025-07-01 12:31:51 +0530

    modified

--- user-service/app.py
+++ user-service/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = [{"id": 1, "name": "John Doe"}, {"id": 2, "name": "Jane Doe"}]
-    return jsonify(users)
+REQUEST_COUNT = Counter('user_service_requests_total', 'Total requests to user service')
+
+@app.route('/user', methods=['GET'])
+def get_products():
+    REQUEST_COUNT.inc()
+    products = [
+        {"product_id": 101, "name": "Product 1"},
+        {"product_id": 102, "name": "Product 2"}
+    ]
+    return jsonify(products)
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5003, debug=True)
+
